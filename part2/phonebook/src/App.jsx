9296781540c4a5e204dee3544e7eccb5b80ae1ed
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-22 17:46:22 -0500

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [filteredPersons, setFilteredPersons] = useState(persons);
+  const [filter, setFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const handleFilter = ($event) => {
+    setFilter($event.target.value);
+    const filter = $event.target.value;
+    if (filter) {
+      const filteredPersons = persons.filter((person) => {
+        const personName = person.name.toLowerCase();
+        const filterToLowerCase = filter.toLowerCase();
+        return personName.includes(filterToLowerCase);
+      });
+      setFilteredPersons([...filteredPersons]);
+    } else {
+      setFilteredPersons([...persons]);
+    }
+  };
   const handleNewName = ($event) => {
     setNewName($event.target.value);
   };
@@ -24,6 +45,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={filter} onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -38,8 +63,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({ name, number }) => (
-        <p key={name}>
+      {filteredPersons.map(({ id, name, number }) => (
+        <p key={id}>
           {name} {number}
         </p>
       ))}
