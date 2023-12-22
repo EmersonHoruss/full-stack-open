ae5dac908b336866f705fdcbf2e444f5f024e18f
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-21 21:55:48 -0500

    1.3: course information step3

--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -4,7 +4,7 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {props.part1.exercises + props.part2.exercises + props.part3.exercises}
     </p>
   );
 };
