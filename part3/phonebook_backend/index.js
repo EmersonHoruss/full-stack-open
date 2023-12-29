588a0d2c8751c3e9f06a6ab143fc302cf695f976
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-28 22:09:45 -0500

    exercises 3.1-3.8

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const morgan = require('morgan');
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'));
+morgan.token('req-body', (req) => {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body);
+    }
+    return null;
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body', {
+    skip: (req) => req.method !== 'POST',
+}));
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    if (!person.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!person.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    const numberAlreadyExist = persons.find(currentPerson => currentPerson.number === person.number)
+    if (numberAlreadyExist) {
+        return response.status(400).json({
+            error: 'number must be unique'
+        })
+    }
+    person.id = Math.floor(Math.random() * 100000)
+    persons = persons.concat(person)
+    response.json(person)
+})
+app.get("/info", (request, response) => {
+    response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
