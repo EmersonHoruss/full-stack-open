bd7317f2f0b010b37d3474c3976cb5d9dff5a7bf
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-31 01:32:21 -0500

    4.4: helper functions and unit tests, step2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,5 +1,7 @@
 const dummy = () => 1;
-
+const totalLikes = (blogs) =>
+  blogs && blogs.length ? blogs.map((blog) => blog.likes).reduce((sum, item) => sum + item) : 0;
 module.exports = {
   dummy,
+  totalLikes,
 };
