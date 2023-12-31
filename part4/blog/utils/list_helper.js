d12b3c5e25b95b85a45b3f18389a6fd9ed8766a2
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2023-12-31 01:46:29 -0500

    4.6*: helper functions and unit tests, step4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,11 @@
+const _ = require('lodash');
+
 const dummy = () => 1;
 const totalLikes = (blogs) =>
   blogs && blogs.length ? blogs.map((blog) => blog.likes).reduce((sum, item) => sum + item) : 0;
+const mostBlogs = (blogs) => _.maxBy(blogs, 'likes');
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 };
